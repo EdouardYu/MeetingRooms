f8f5a47795dfe63b9b4bd6e0cf6a92f03ff23fe4
EdouardY <yuedouard@gmail.com>
2022-11-12 01:53:16 +0100

    init

--- server/src/helper.js
+++
@@ -1,9 +0,0 @@
-exports.success = (message, data) => {
-    return { message, data }
-}
-
-exports.pluralize = (count, singular, plural) => {
-    const correctStringVersion = count == 1 ? singular : plural;
-    const correctString = count + ' ' + correctStringVersion
-    return correctString;
-}
